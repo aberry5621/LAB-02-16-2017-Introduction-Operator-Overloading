ac05e9158cf13b5a061e8af3844c6caefe88f200
aberry5621 <aberry5621@gmail.com>
2017-03-20 17:21:59 -0700

    Initial Commit

---
+++ LAB 02 16 2017 Introduction Operator Overloading/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LAB 02 16 2017 Introduction Operator Overloading
+//
+//  Created by ax on 3/20/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
